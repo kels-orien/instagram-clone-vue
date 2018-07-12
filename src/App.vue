c8bfa47d5767cfb87d8d7b7b9ea6cf0084828112
kels-orien <kelvinorien@gmail.com>
2018-07-12 21:51:17 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    
+
+ <section class="app">
+    <headers></headers>
+     </section> 
+    </div>
+     
+</template>
+
+<script>
+import Headers from './components/Headers'
+export default {
+  name: 'App',
+  components: {
+      Headers
+    }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+.app {
+  background: #fafafaa8;
+  min-height: 100vh;
+  min-width: 100vh;
+}
+ .main {
+  width: 50%;
+  margin: 0 auto;
+}
+.Nav {
+  background-color: #fff;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.0975);
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 2;
+  -webkit-transition: height 0.2s ease-in-out;
+  transition: height 0.2s ease-in-out;
+  height: 77px;
+}
+
+.Nav-menus {
+  display: flex;
+  flex-direction: row;
+  height: 77px;
+  /* justify-content: center; */
+  width: 70%;
+  margin: 0 auto;
+  padding: 26px 40px;
+}
+
+.Nav-brand {
+  /* height: auto; */
+}
+
+.Nav-brand-logo {
+  display: block;
+  background-position: -98px -317px;
+  background-image: url(./assets/instagram-sprite.png);
+  background-repeat: no-repeat;
+  height: 35px;
+  width: 176px;
+  text-indent: -2000%
+}
+</style>
